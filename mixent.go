544e4104b68ab9b6e5e037695e019e68cf139a3b
Brendan Tracey <tracey.brendan@gmail.com>
2017-05-06 18:28:17 -0600

    Initial commit

---
+++ mixent.go
@@ -0,0 +1,282 @@
+// Copyright ©2016 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// mixent implements routines for estimating the entropy of a mixture distribution.
+// See PAPER for more information.
+// Documentation notation: the mixture distribution is
+//  p(x) = \sum_{i=1}^N w_i p_i(x)
+// The symbol X will be used to represent the mixture distribution p(x), and
+// X_i will represent p_i(x), that is the distribution of the i^th mixture component.
+// It will also use \sum_i as a shorthand for \sum_{i=1}^N.
+//
+// Please note that some of the types implemented here work for all mixture
+// distributions, while others only work for special cases, such as mixture of
+// Gaussians.
+package mixent
+
+import (
+	"math"
+
+	"github.com/gonum/floats"
+	"github.com/gonum/matrix/mat64"
+	"github.com/gonum/stat"
+	"github.com/gonum/stat/distmv"
+)
+
+var (
+	dimensionMismatch = "mixent: component dimension mismatch"
+)
+
+// Component is an individual component in a mixture distribution.
+type Component interface {
+	Entropy() float64
+}
+
+// Estimator is a type that can estimate the entropy of a mixture of Components.
+type Estimator interface {
+	MixtureEntropy(components []Component, weights []float64) float64
+}
+
+// Distancer is a type that can compute the distance between two components.
+// The distance returned must be greater than zero, and must equal zero if a and
+// b are identical distributions.
+type Distancer interface {
+	Distance(a, b Component) float64
+}
+
+// NormalDistance wraps a DistNormaler for use with components.
+type NormalDistance struct {
+	distmv.DistNormaler
+}
+
+// Distance computes the distance between two Normal components. Distance panics
+// if the underlying type of the components is not *distmv.Normal or if the
+// dimensions are unequal.
+func (n NormalDistance) Distance(l, r Component) float64 {
+	l1 := l.(*distmv.Normal)
+	l2 := r.(*distmv.Normal)
+	return n.DistNormal(l1, l2)
+}
+
+// NormalDistance wraps a DistUniformer for use with components.
+type UniformDistance struct {
+	distmv.DistUniformer
+}
+
+// Distance computes the distance between two Normal components. Distance panics
+// if the underlying type of the components is not *distmv.Uniform or if the
+// dimensions are unequal.
+func (u UniformDistance) Distance(l, r Component) float64 {
+	l1 := l.(*distmv.Uniform)
+	l2 := r.(*distmv.Uniform)
+	return u.DistUniform(l1, l2)
+}
+
+// LowerGaussian is a lower bound on the entropy of a mixture of Gaussians.
+// See the MixtureEntropy method for more information.
+type LowerGaussian struct{}
+
+// MixtureEntropy computes a lower bound on the entropy of a mixture of Gaussians.
+// It implements Section V.A, Theorem 2 of
+//  Huber, Marco F., et al. "On entropy approximation for Gaussian mixture
+//  random vectors." Multisensor Fusion and Integration for Intelligent Systems,
+//  2008. MFI 2008. IEEE International Conference on. IEEE, 2008.
+// The entropy estimate is
+//  H(x) ≈ -\sum_i w_i log(\sum_j w_j z_{i,j})
+//  z_{i,j} = 𝒩(μ_i; μ_j, Σ_i+Σ_j)
+// All of the mixture components must be *distmv.Normal and must have the same
+// input dimension or MixtureEntropy will panic.
+//
+// If weights is nil, all components are assumed to have the same mixture entropy.
+func (l LowerGaussian) MixtureEntropy(components []Component, weights []float64) float64 {
+	n := len(components)
+
+	// Convert the Component to *distmv.Normal and check the dimensions.
+	norms := make([]*distmv.Normal, n)
+	norms[0] = components[0].(*distmv.Normal)
+	dim := norms[0].Dim()
+	for i := 1; i < n; i++ {
+		norm := components[i].(*distmv.Normal)
+		if norm.Dim() != dim {
+			panic(dimensionMismatch)
+		}
+		norms[i] = norm
+	}
+	if weights == nil {
+		weights = make([]float64, n)
+		for i := range weights {
+			weights[i] = 1 / float64(n)
+		}
+	}
+
+	// Create memory storage.
+	mui := make([]float64, dim)
+	muj := make([]float64, dim)
+	covi := mat64.NewSymDense(dim, nil)
+	covj := mat64.NewSymDense(dim, nil)
+	covSum := mat64.NewSymDense(dim, nil)
+	inner := make([]float64, n)
+
+	// Estimate entropy.
+	var ent float64
+	for i, a := range norms {
+		a.Mean(mui)
+		a.CovarianceMatrix(covi)
+		for j, b := range norms {
+			b.Mean(muj)
+			b.CovarianceMatrix(covj)
+			covSum.AddSym(covi, covj)
+			norm, ok := distmv.NewNormal(muj, covSum, nil)
+			if !ok {
+				// This should be impossible because the sum of PSD matrices is PSD.
+				panic("mixent: covariance sum not positive definite")
+			}
+			inner[j] = norm.LogProb(mui) + math.Log(weights[j])
+		}
+		ent -= weights[i] * floats.LogSumExp(inner)
+	}
+	return ent
+}
+
+// JointEntropy provides an upper bound on the mixture entropy using
+// the joint entropy of the mixture and the weights. See the MixtureEntropy
+// method for more information.
+type JointEntropy struct{}
+
+// MixtureEntropy computes an upper bound to the mixture entropy for arbitrary
+// mixture components.
+//
+// The joint entropy between the mixture distribution and the weight distribution
+// is an upper bound on the entropy of the distribution
+//  H(X) ≤ H(X, W) = H(X|W) + H(W) = \sum_i w_i H(p_i) + H(W)
+//
+// If weights is nil, all components are assumed to have the same mixture entropy.
+func (JointEntropy) MixtureEntropy(components []Component, weights []float64) float64 {
+	avgEnt := AvgEnt{}.MixtureEntropy(components, weights)
+	if weights == nil {
+		return avgEnt + math.Log(float64(len(components)))
+	}
+	avgEnt += stat.Entropy(weights)
+	return avgEnt
+}
+
+// AvgEnt provides a lower bound on the mixture entropy using the average entropy
+// of the components. See the MixtureEntropy method for more information.
+type AvgEnt struct{}
+
+// MixtureEntropy computes the entropy of the mixture conditional on the mixture
+// weight, which is a lower bound to the true entropy of the mixture distribution.
+//  H(X) ≥ H(X|W) = \sum_i w_i H(X_i)
+// i.e. the average entropy of the components.
+//
+// If weights is nil, all components are assumed to have the same mixture entropy.
+func (AvgEnt) MixtureEntropy(components []Component, weights []float64) float64 {
+	if weights == nil {
+		var avgEnt float64
+		nf := float64(len(components))
+		for _, v := range components {
+			avgEnt += v.Entropy() / nf
+		}
+		return avgEnt
+	}
+	var avgEnt float64
+	for i, v := range components {
+		avgEnt += weights[i] * v.Entropy()
+	}
+	return avgEnt
+}
+
+// PairwiseDistance estimates the entropy using a pairwise distance metric.
+// See the MixtureEntropy method for more information.
+type PairwiseDistance struct {
+	Distancer Distancer
+}
+
+// MixtureEntropy estimates the entropy using a distance metric between each pair
+// of distributions. It implements EQUATION IN PAPER, that is
+//  H(x) ≈ \sum_i w_i H(X_i) - \sum_i w_i ln(\sum_j w_j exp(-D(X_i || X_j)))
+// As shown in Kolchinsky, Tracey and Wolpert this estimator has several nice
+// properties (see the paper for necessary conditions on D).
+//  CITATION
+// 1) The estimate returned is always larger than Conditional and smaller than
+// JointMixtureWeight. These two estimators differ by H(weights), so this provides
+// a bound on the error.
+//
+// 2) The pairwise estimator becomes an exact estimate of the entropy as the mixture
+// components become clustered. The number of clusters is arbitrary, so this estimator
+// is exact when all mixture components are the same, all are very far apart from
+// one another, or clustered into k far apart clusters.
+//
+// 3) The Bhattacharrya distance metric is a lower-bound on the mixture entropy
+// and the Kullback-Leibler divergence is an upper bound on the mixture entropy.
+// Any distance metric D_B ≤ D ≤ D_KL will provide an estimate in between these
+// two distances.
+//
+// If weights is nil, all components are assumed to have the same mixture entropy.
+func (p PairwiseDistance) MixtureEntropy(components []Component, weights []float64) float64 {
+	n := len(components)
+	if weights == nil {
+		weights = make([]float64, n)
+		for i := range weights {
+			weights[i] = 1 / float64(n)
+		}
+	}
+	var avgEntropy float64
+	for i, v := range components {
+		avgEntropy += weights[i] * v.Entropy()
+	}
+
+	var outer float64
+	inner := make([]float64, n) // Storing the sums for numeric stability
+	for i, xi := range components {
+		for j, xj := range components {
+			inner[j] = math.Log(weights[j]) - p.Distancer.Distance(xi, xj)
+		}
+		outer += weights[i] * floats.LogSumExp(inner)
+	}
+	return avgEntropy - outer
+}
+
+// ComponentCenters estimates the entropy based on the probability at the component
+// centers.
+type ComponentCenters struct{}
+
+// MixtureEntropy computes the estimate of the entropy based on the average
+// probability of the cluster centers.
+//  H(x) ≈ - \sum_i w_i ln \sum_j w_j p_j(μ_i)
+// If weights is nil, all components are assumed to have the same mixture entropy.
+//
+// Currently only coded for Gaussian components.
+func (p ComponentCenters) MixtureEntropy(components []Component, weights []float64) float64 {
+	n := len(components)
+	norms := make([]*distmv.Normal, n)
+	norms[0] = components[0].(*distmv.Normal)
+	dim := norms[0].Dim()
+	for i := 1; i < n; i++ {
+		norm := components[i].(*distmv.Normal)
+		if norm.Dim() != dim {
+			panic(dimensionMismatch)
+		}
+		norms[i] = norm
+	}
+	if weights == nil {
+		weights = make([]float64, n)
+		for i := range weights {
+			weights[i] = 1 / float64(n)
+		}
+	}
+
+	// Compute center probabilities
+	inner := make([]float64, n)
+	var ent float64
+	mean := make([]float64, dim)
+	for i, norm := range norms {
+		norm.Mean(mean)
+		for j, norm2 := range norms {
+			inner[j] = math.Log(weights[j]) + norm2.LogProb(mean)
+		}
+		ent += weights[i] * floats.LogSumExp(inner)
+	}
+	return -ent
+}
---
+++ paperplots/paperplots.go
@@ -0,0 +1,699 @@
+// Copyright ©2016 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"log"
+	"math"
+	"math/rand"
+
+	"github.com/btracey/mixent"
+	"github.com/btracey/myplot"
+	"github.com/gonum/floats"
+	"github.com/gonum/matrix"
+	"github.com/gonum/matrix/mat64"
+	"github.com/gonum/plot"
+	"github.com/gonum/plot/plotter"
+	"github.com/gonum/plot/plotutil"
+	"github.com/gonum/plot/vg"
+	"github.com/gonum/plot/vg/draw"
+	"github.com/gonum/stat/distmv"
+	"github.com/gonum/stat/distuv"
+)
+
+func main() {
+	names := []string{
+		"gauss_sweep_skew",
+		"gauss_sweep_size",
+		"gauss_sweep_clusters",
+		"gauss_sweep_dim",
+
+		"unif_sweep_size",
+		"unif_sweep_skew",
+		"unif_sweep_clusters",
+		"unif_sweep_dim",
+	}
+
+	plot.DefaultFont = "Helvetica"
+
+	for _, name := range names {
+		fmt.Println("Running ", name)
+		run := GetRun(name)
+
+		// Fix the random samples for each run. Keep the randomness consistent
+		// across the hyper sweep to reduce noise.
+		rnd := rand.New(rand.NewSource(1))
+
+		// The code allows the dimension of the problem to adjust with the
+		// hyperparameters. Find the maximum dimension used.
+		var maxDim int
+		for _, hyper := range run.Hypers {
+			dim := run.DistGen.CompDim(hyper)
+			if dim > maxDim {
+				maxDim = dim
+			}
+		}
+
+		// Generate random samples for computing the MC entropy.
+		randComps := make([]int, run.MCEntropySamples)
+		for i := range randComps {
+			randComps[i] = rnd.Intn(run.NumComponents)
+		}
+		mcSamps := mat64.NewDense(run.MCEntropySamples, maxDim, nil)
+		for i := 0; i < run.MCEntropySamples; i++ {
+			for j := 0; j < maxDim; j++ {
+				mcSamps.Set(i, j, rnd.Float64())
+			}
+		}
+
+		// Generate the random numbers for components.
+		nRand := run.DistGen.NumRandom()
+		compSamps := mat64.NewDense(run.NumComponents, nRand, nil)
+		for i := 0; i < run.NumComponents; i++ {
+			for j := 0; j < nRand; j++ {
+				compSamps.Set(i, j, rnd.Float64())
+			}
+		}
+
+		// Sweep over the hyperparameter, and estimate the entropy with all of the
+		// estimators
+		entComponents := make([]mixent.Component, run.NumComponents)
+		components := make([]Component, run.NumComponents)
+		mcEnt := make([]float64, len(run.Hypers)) // store of the entropy from Monte Carlo.
+
+		estEnts := mat64.NewDense(len(run.Hypers), len(run.Estimators), nil) // entropy from estimators.
+		for i, hyper := range run.Hypers {
+			fmt.Println(name, i, "of", len(run.Hypers))
+
+			// Construct the components given the random samples and hyperparameter.
+			for j := range components {
+				components[j] = run.DistGen.ComponentFrom(compSamps.RawRowView(j), hyper)
+				entComponents[j] = components[j]
+			}
+
+			// Estimate the entropy with all the estimators.
+			for j, estimator := range run.Estimators {
+				v := estimator.MixtureEntropy(entComponents, nil)
+				estEnts.Set(i, j, v)
+			}
+
+			// Estimate the entropy from Monte Carlo.
+			dim := run.DistGen.CompDim(hyper)
+			sv := mcSamps.Slice(0, run.MCEntropySamples, 0, dim).(*mat64.Dense)
+			mcEnt[i] = mcEntropy(components, randComps, sv)
+		}
+
+		// Plot the results.
+		err := makePlots(run, mcEnt, estEnts)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// mcEntropy estimates the entropy of the mixture distribution given the pre-drawn
+// random components and samples.
+func mcEntropy(components []Component, randComps []int, mcSamps *mat64.Dense) float64 {
+	nSamp, dim := mcSamps.Dims()
+	if len(randComps) != nSamp {
+		panic("rand mismatch")
+	}
+	var ent float64
+	lw := -math.Log(float64(len(components))) // probability of chosing component.
+	x := make([]float64, dim)
+	lps := make([]float64, len(components))
+	for i := range randComps {
+		// Extract the sampled x location.
+		comp := randComps[i]
+		components[comp].Quantile(x, mcSamps.RawRowView(i))
+		// Compute \sum_i w_i p(x_i).
+		for j := range lps {
+			lps[j] = components[j].LogProb(x) + lw
+		}
+		ent += floats.LogSumExp(lps)
+	}
+	return -ent / float64(nSamp)
+}
+
+type Run struct {
+	Name             string
+	DistGen          DistributionGenerator
+	MCEntropySamples int
+	NumComponents    int
+
+	Hypers     []float64
+	Estimators []mixent.Estimator
+
+	XLabel string
+	LogX   bool
+}
+
+func GetRun(name string) Run {
+	var isUniform bool
+
+	c := Run{
+		Name:             name,
+		MCEntropySamples: 3000,
+		NumComponents:    100,
+	}
+
+	switch name {
+	default:
+		panic("unknown case name")
+	case "gauss_sweep_size":
+		dim := 10
+		hypers := make([]float64, 30)
+		floats.Span(hypers, -2, 3)
+		for i, v := range hypers {
+			hypers[i] = math.Exp(v)
+		}
+
+		c.DistGen = ShiftCenters{dim, false}
+		c.Hypers = hypers
+		c.XLabel = "Ln(Mean Spread)"
+		c.LogX = true
+	case "gauss_sweep_skew":
+		dim := 10
+		hypers := make([]float64, 30)
+		floats.Span(hypers, 0, 7)
+		for i, v := range hypers {
+			hypers[i] = math.Exp(v)
+		}
+
+		c.DistGen = GaussianFixedCenter{dim}
+		c.Hypers = hypers
+		c.XLabel = "Ln(Covariance Similarity)"
+		c.LogX = true
+	case "gauss_sweep_clusters":
+		dim := 10
+		hypers := make([]float64, 30)
+		floats.Span(hypers, -2, 1)
+		for i, v := range hypers {
+			hypers[i] = math.Exp(v)
+		}
+
+		// Geterate centers from uniform ball
+		nClusters := 5
+		centers := mat64.NewDense(nClusters, dim, nil)
+		for i := 0; i < nClusters; i++ {
+			for j := 0; j < dim; j++ {
+				centers.Set(i, j, rand.NormFloat64())
+			}
+		}
+
+		c.DistGen = ClusterCenters{centers, false}
+		c.Hypers = hypers
+		c.XLabel = "Ln(Cluster Spread)"
+		c.LogX = true
+	case "gauss_sweep_dim":
+		hypers := make([]float64, 8)
+		floats.Span(hypers, 1, 60)
+		maxDim := int(hypers[len(hypers)-1])
+
+		c.DistGen = FlexibleDim{maxDim, false}
+		c.Hypers = hypers
+		c.XLabel = "Dimension"
+		c.LogX = false
+	case "unif_sweep_size":
+		isUniform = true
+		dim := 10
+		hypers := make([]float64, 30)
+		floats.Span(hypers, -4, 1.5)
+		for i, v := range hypers {
+			hypers[i] = math.Exp(v)
+		}
+
+		c.DistGen = ShiftCenters{dim, true}
+		c.Hypers = hypers
+		c.XLabel = "Ln(Mean Spread)"
+		c.LogX = true
+	case "unif_sweep_clusters":
+		isUniform = true
+		dim := 10
+		hypers := make([]float64, 30)
+		floats.Span(hypers, -6, 0)
+		for i, v := range hypers {
+			hypers[i] = math.Exp(v)
+		}
+
+		// Geterate centers from uniform ball
+		nClusters := 5
+		centers := mat64.NewDense(nClusters, dim, nil)
+		for i := 0; i < nClusters; i++ {
+			for j := 0; j < dim; j++ {
+				centers.Set(i, j, rand.NormFloat64())
+			}
+		}
+
+		c.DistGen = ClusterCenters{centers, true}
+		c.Hypers = hypers
+		c.XLabel = "Ln(Cluster Spread)"
+		c.LogX = true
+	case "unif_sweep_dim":
+		isUniform = true
+		hypers := make([]float64, 8)
+		floats.Span(hypers, 1, 22)
+		maxDim := int(hypers[len(hypers)-1])
+
+		c.DistGen = FlexibleDim{maxDim, true}
+		c.Hypers = hypers
+		c.XLabel = "Dimension"
+		c.LogX = false
+	case "unif_sweep_skew":
+		isUniform = true
+		dim := 10
+		hypers := make([]float64, 30)
+		floats.Span(hypers, -5, 5)
+		for i, v := range hypers {
+			hypers[i] = math.Exp(v)
+		}
+
+		c.DistGen = UniformFixedCenter{dim}
+		c.Hypers = hypers
+		c.XLabel = "Ln(Width Variability)"
+		c.LogX = true
+	}
+	if isUniform {
+		c.Estimators = []mixent.Estimator{
+			mixent.JointEntropy{},
+			mixent.AvgEnt{},
+			mixent.PairwiseDistance{mixent.UniformDistance{distmv.KullbackLeibler{}}},
+			mixent.PairwiseDistance{mixent.UniformDistance{distmv.Bhattacharyya{}}},
+		}
+	} else {
+		c.Estimators = []mixent.Estimator{
+			mixent.JointEntropy{},
+			mixent.AvgEnt{},
+			mixent.LowerGaussian{},
+			mixent.PairwiseDistance{mixent.NormalDistance{distmv.KullbackLeibler{}}},
+			mixent.PairwiseDistance{mixent.NormalDistance{distmv.Bhattacharyya{}}},
+			mixent.ComponentCenters{},
+		}
+	}
+	return c
+}
+
+// Component is the same as a mixent.Component, except can also compute probabilities
+// and quantiles
+type Component interface {
+	mixent.Component
+	distmv.Quantiler
+	distmv.LogProber
+}
+
+// DistributionGenerator is a type for generating the Components of a mixture model.
+type DistributionGenerator interface {
+	// NumRandom is the amount of random numbers needed to generate a component
+	// (random location of the mean, etc.)
+	NumRandom() int
+	// ComponentFrom takes a set of random numbers and turns it into a component
+	ComponentFrom(rands []float64, hyper float64) Component
+	// CompDim returns the dimension as a function of the hyperparameter.
+	CompDim(hyper float64) int
+}
+
+// ShiftCenters generates fixed-width components with center locations randomly
+// generated on the hypercube with size hyper.
+type ShiftCenters struct {
+	Dim     int
+	Uniform bool // if true, Uniform components. If false, Gaussian.
+}
+
+func (n ShiftCenters) CompDim(hyper float64) int {
+	return n.Dim
+}
+
+func (n ShiftCenters) NumRandom() int {
+	return n.Dim
+}
+
+func (n ShiftCenters) ComponentFrom(rands []float64, hyper float64) Component {
+	if len(rands) != n.Dim {
+		panic("bad ")
+	}
+	mean := make([]float64, len(rands))
+	for i := range mean {
+		mean[i] = rands[i] * hyper
+	}
+	if n.Uniform {
+		bounds := make([]distmv.Bound, n.Dim)
+		for i := range bounds {
+			bounds[i].Min = mean[i] - 0.5
+			bounds[i].Max = mean[i] + 0.5
+		}
+		return distmv.NewUniform(bounds, nil)
+	}
+	cov := mat64.NewSymDense(n.Dim, nil)
+	for i := 0; i < n.Dim; i++ {
+		cov.SetSym(i, i, 1)
+	}
+	norm, ok := distmv.NewNormal(mean, cov, nil)
+	if !ok {
+		panic("bad covariance")
+	}
+	return norm
+}
+
+// ClusterCenters generates components according to their cluster. The hyperparameter
+// controls the spread of the cluster locations.
+type ClusterCenters struct {
+	Centers *mat64.Dense
+	Uniform bool // if true, Uniform components. If false, Gaussian.
+}
+
+func (n ClusterCenters) CompDim(hyper float64) int {
+	_, dim := n.Centers.Dims()
+	return dim
+}
+
+func (n ClusterCenters) NumRandom() int {
+	// Which cluster
+	return 1
+}
+
+func (n ClusterCenters) ComponentFrom(rands []float64, hyper float64) Component {
+	nClust, dim := n.Centers.Dims()
+	// Get the number of the cluster
+	v := rands[0]
+	v *= float64(nClust)
+	idx := int(math.Floor(v))
+
+	mean := make([]float64, dim)
+	copy(mean, n.Centers.RawRowView(idx))
+
+	for i := range mean {
+		mean[i] *= hyper
+	}
+	if n.Uniform {
+		bounds := make([]distmv.Bound, len(mean))
+		for i := range bounds {
+			bounds[i].Min = mean[i] - 0.5
+			bounds[i].Max = mean[i] + 0.5
+		}
+		return distmv.NewUniform(bounds, nil)
+	}
+	cov := mat64.NewSymDense(dim, nil)
+	for i := 0; i < dim; i++ {
+		cov.SetSym(i, i, 1)
+	}
+	norm, ok := distmv.NewNormal(mean, cov, nil)
+	if !ok {
+		panic("bad covariance")
+	}
+	return norm
+}
+
+// GaussianFixedCenter generates Gaussian components with a fixed center location
+// and covariance drawn from a Wishart distribution. As the hyperparameter increases,
+// the covariances tend to the identity matrix.
+type GaussianFixedCenter struct {
+	Dim int
+}
+
+func (w GaussianFixedCenter) CompDim(hyper float64) int {
+	return w.Dim
+}
+
+func (w GaussianFixedCenter) NumRandom() int {
+	// Center location + chi^2 variables + normal variables
+	return w.Dim + w.Dim + (w.Dim-1)*w.Dim/2
+}
+
+func (w GaussianFixedCenter) ComponentFrom(rands []float64, hyper float64) Component {
+	dim := w.Dim
+	nu := float64(dim) + hyper
+
+	// The v matrix is Sigma_0^-1, and Sigma_0 = nu*I. So v = 1/nu * I. We are
+	// computing the cholesky decomposition, so the square root of that
+	cholv := mat64.NewTriDense(dim, matrix.Upper, nil)
+	for i := 0; i < dim; i++ {
+		cholv.SetTri(i, i, math.Sqrt(1/nu))
+	}
+
+	// The center is just fixed, the first rands.
+	mu := rands[:dim]
+	rands = rands[dim:]
+
+	u := mat64.NewTriDense(dim, matrix.Upper, nil)
+	// Get the chi^2 random variables. Diagonal generated from Chi^2 of nu-i
+	// degrees of freedom.
+	for i := 0; i < dim; i++ {
+		v := distuv.ChiSquared{
+			K:   nu - float64(i),
+			Src: nil,
+		}.Quantile(rands[i])
+		u.SetTri(i, i, math.Sqrt(v))
+	}
+
+	// Get the normal random variables for the off-diagonal
+	rands = rands[dim:]
+	for i := 0; i < dim; i++ {
+		for j := i + 1; j < dim; j++ {
+			u.SetTri(i, j, distuv.UnitNormal.Quantile(rands[0]))
+			rands = rands[1:]
+		}
+	}
+
+	var t mat64.TriDense
+	t.MulTri(u, cholv)
+
+	var c mat64.Cholesky
+	c.SetFromU(&t)
+	var cov mat64.SymDense
+	cov.FromCholesky(&c)
+
+	// TODO(btracey): Can set directly from Cholesky.
+	norm, ok := distmv.NewNormal(mu, &cov, nil)
+	if !ok {
+		panic("bad norm")
+	}
+	return norm
+}
+
+// UniformFixedCenter generates Uniform components with a fixed center location
+// and widths drawn from a Gamma distribution. As the hyperparameter increases,
+// the widths tend to unity.
+type UniformFixedCenter struct {
+	Dim int
+}
+
+func (u UniformFixedCenter) CompDim(hyper float64) int {
+	return u.Dim
+}
+
+func (u UniformFixedCenter) NumRandom() int {
+	// Center location + Gamma quantile.
+	return 2 * u.Dim
+}
+
+func (u UniformFixedCenter) ComponentFrom(rands []float64, hyper float64) Component {
+	bounds := make([]distmv.Bound, u.Dim)
+	for i := 0; i < len(rands); i += 2 {
+		// mean is the first random variable
+		mean := rands[i] - 0.5
+		// width is second
+		width := distuv.Gamma{Alpha: 1 + hyper, Beta: 1 + hyper}.Quantile(rands[i+1])
+
+		bounds[i/2].Max = mean + width/2
+		bounds[i/2].Min = mean - width/2
+	}
+	return distmv.NewUniform(bounds, nil)
+}
+
+// FlexibleDim generates components whose dimension increases with the hyperparameter.
+type FlexibleDim struct {
+	MaxDim  int
+	Uniform bool // if true, Uniform components. If false, Gaussian.
+}
+
+func (f FlexibleDim) CompDim(hyper float64) int {
+	return int(hyper)
+}
+
+func (f FlexibleDim) NumRandom() int {
+	return f.MaxDim
+}
+
+func (f FlexibleDim) ComponentFrom(rands []float64, hyper float64) Component {
+	dim := f.CompDim(hyper)
+
+	// Center is the first dim entries
+	mean := make([]float64, dim)
+	copy(mean, rands)
+
+	if f.Uniform {
+		bounds := make([]distmv.Bound, len(mean))
+		for i := range bounds {
+			bounds[i].Min = 2 * (mean[i] - 0.5)
+			bounds[i].Max = 2 * (mean[i] + 0.5)
+		}
+		return distmv.NewUniform(bounds, nil)
+	}
+
+	sigma := eyeSym(dim)
+	n, ok := distmv.NewNormal(mean, sigma, nil)
+	if !ok {
+		panic("bad covariance")
+	}
+	return n
+}
+
+func eyeSym(dim int) *mat64.SymDense {
+	m := mat64.NewSymDense(dim, nil)
+	for i := 0; i < dim; i++ {
+		m.SetSym(i, i, 1)
+	}
+	return m
+}
+
+func makePlots(run Run, mcEnt []float64, estEnts *mat64.Dense) error {
+	// Collect all of the data into lines
+	x := make([]float64, len(run.Hypers))
+	copy(x, run.Hypers)
+	if run.LogX {
+		for i, v := range x {
+			x[i] = math.Log(v)
+		}
+	}
+	minEnt := floats.Min(mcEnt)
+	maxEnt := floats.Max(mcEnt)
+
+	l, err := plotter.NewLine(myplot.VecXY{x, mcEnt})
+	if err != nil {
+		return err
+	}
+	linedata := []interface{}{"Monte Carlo", l}
+
+	for j, estimator := range run.Estimators {
+		y := mat64.Col(nil, j, estEnts)
+		min := floats.Min(y)
+		if min < minEnt {
+			minEnt = min
+		}
+		max := floats.Max(y)
+		if max > maxEnt {
+			maxEnt = max
+		}
+
+		if _, ok := estimator.(mixent.JointEntropy); ok {
+			continue
+		}
+		if _, ok := estimator.(mixent.AvgEnt); ok {
+			continue
+		}
+
+		l, err := plotter.NewLine(myplot.VecXY{x, y})
+		if err != nil {
+			return err
+		}
+		name, ls := plotMapName(estimator)
+		l.LineStyle = ls
+		linedata = append(linedata, name)
+		linedata = append(linedata, l)
+	}
+
+	p, err := plot.New()
+	if err != nil {
+		return err
+	}
+
+	// Draw a polygon with the HuberUpper and AvgEnt
+	polyXYs := make(plotter.XYs, len(x)*2)
+	for i, est := range run.Estimators {
+		_, ok := est.(mixent.JointEntropy)
+		if ok {
+			for j, v := range x {
+				polyXYs[j].X = v
+				polyXYs[j].Y = estEnts.At(j, i)
+			}
+		}
+	}
+	for i, est := range run.Estimators {
+		_, ok := est.(mixent.AvgEnt)
+		if ok {
+			for j, v := range x {
+				polyXYs[2*len(x)-j-1].X = v
+				polyXYs[2*len(x)-j-1].Y = estEnts.At(j, i)
+			}
+		}
+	}
+	poly, err := plotter.NewPolygon(polyXYs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	polyColor := color.RGBA{242, 251, 222, 255}
+	poly.Color = polyColor
+	poly.LineStyle.Color = polyColor
+	p.Add(poly)
+
+	makeLegend := run.Name == "gauss_sweep_dim" || run.Name == "unif_sweep_dim"
+
+	for i := 0; i < len(linedata); i += 2 {
+		p.Add(linedata[i+1].(plot.Plotter))
+		if makeLegend {
+			p.Legend.Add(linedata[i].(string), linedata[i+1].(plot.Thumbnailer))
+		}
+	}
+	if makeLegend {
+		p.Legend.Add("[H(X|C), H(X,C)]", poly)
+		p.Legend.Top = true
+		p.Legend.Left = true
+	}
+
+	p.X.Label.Text = run.XLabel
+	p.Y.Label.Text = "Entropy (nats)"
+
+	p.Y.Max = (maxEnt-minEnt)*1.05 + minEnt
+	p.Y.Min = maxEnt + (minEnt-maxEnt)*1.05
+
+	err = p.Save(5*vg.Inch, 3*vg.Inch, "plot_"+run.Name+".pdf")
+	return err
+}
+
+func plotMapName(e mixent.Estimator) (string, draw.LineStyle) {
+	ls := plotter.DefaultLineStyle
+	switch t := e.(type) {
+	default:
+		panic("unknown type")
+	case mixent.JointEntropy:
+		ls.Color = plotutil.Color(1)
+		ls.Dashes = plotutil.Dashes(1)
+		return "Huber Upper", ls
+	case mixent.LowerGaussian:
+		ls.Color = plotutil.Color(2)
+		ls.Dashes = plotutil.Dashes(2)
+		return "Huber Lower", ls
+	case mixent.AvgEnt:
+		ls.Color = plotutil.Color(3)
+		ls.Dashes = plotutil.Dashes(3)
+		return "Avg. Ent.", ls
+	case mixent.ComponentCenters:
+		ls.Color = plotutil.Color(4)
+		ls.Dashes = plotutil.Dashes(4)
+		return "KDE", ls
+	case mixent.PairwiseDistance:
+		var inner interface{}
+		switch v := t.Distancer.(type) {
+		case mixent.NormalDistance:
+			inner = v.DistNormaler
+		case mixent.UniformDistance:
+			inner = v.DistUniformer
+		}
+		switch inner.(type) {
+		default:
+			panic("unknown type")
+		case distmv.KullbackLeibler:
+			ls.Color = plotutil.Color(5)
+			ls.Dashes = plotutil.Dashes(5)
+			return "Pair Dist: KL", ls
+		case distmv.Bhattacharyya:
+			ls.Color = plotutil.Color(6)
+			ls.Dashes = plotutil.Dashes(6)
+			return "Pair Dist: Bhat.", ls
+		}
+	}
+}
